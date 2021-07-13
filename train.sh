7d1d5963929ca66c7d7f13db38b30a9a1f5bbb48
Reyn4Bo <72241110+Reyn4Bo@users.noreply.github.com>
2021-07-13 15:48:01 -0500

    Delete train.sh

--- train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python train_cross_val.py --epochs 60 --batch-size 32  --data imageCLEF_cross_val.yaml --weights yolov5s.pt --single-cls --sideloss
-
-#python train_cross_val.py --epochs 30 --batch-size 32  --data imageCLEF_cross_val.yaml --weights yolov5s.pt --single-cls
